e9a2ab8ade6ef678035b19b43124ecf02922fe50
NARESHKUMAR GOUD <90695464+naresh-goud@users.noreply.github.com>
2021-10-27 00:22:07 +0530

    creating trivia component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./app.css";
+import Trivia from "./components/Trivia";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
@@ -26,7 +27,9 @@ function App() {
         <div className="top">
           <div className="timer">30</div>
         </div>
-        <div className="bottom">question and answers</div>
+        <div className="bottom">
+          <Trivia />
+        </div>
       </div>
       <div className="pyramid">
         <ul className="moneyList">
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">what's the best youtube channel?</div>
+      <div className="answers">
+        <div className="answer correct">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+      </div>
+    </div>
+  );
+}
