825a276175b4f6b530c387d9a976ddc7cea11269
NARESHKUMAR GOUD <90695464+naresh-goud@users.noreply.github.com>
2021-10-27 13:31:15 +0530

    add sound to react app

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../assets/play.mp3";
+import correct from "../assets/correct.mp3";
+import wrong from "../assets/wrong.mp3";
 
 export default function Trivia({
   data,
@@ -9,6 +13,13 @@ export default function Trivia({
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPLay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPLay();
+  }, [letsPLay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -26,12 +37,18 @@ export default function Trivia({
     delay(3000, () =>
       setClassName(a.correct ? "answer correct" : "answer wrong")
     );
-    delay(6000, () => {
+    delay(5000, () => {
       if (a.correct) {
-        setQuestionNumber((prev) => prev + 1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
   };
