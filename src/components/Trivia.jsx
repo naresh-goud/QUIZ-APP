728285b96c7a446d8ee64188909f875d93bd7e22
NARESHKUMAR GOUD <90695464+naresh-goud@users.noreply.github.com>
2021-10-27 10:28:53 +0530

    correct and wrong anser logic

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -14,12 +14,26 @@ export default function Trivia({
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
 
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
   const handleClick = (a) => {
     setSelectedAnswer(a);
     setClassName("answer active");
-    setTimeout(() => {
-      setClassName(a.correct ? "answer correct" : "answer wrong");
-    }, 3000);
+    delay(3000, () =>
+      setClassName(a.correct ? "answer correct" : "answer wrong")
+    );
+    delay(6000, () => {
+      if (a.correct) {
+        setQuestionNumber((prev) => prev + 1);
+        setSelectedAnswer(null);
+      } else {
+        setStop(true);
+      }
+    });
   };
 
   return (
